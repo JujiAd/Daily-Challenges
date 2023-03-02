30c5076ab64364696dcc19ef8fc8560fa35c0836
Jacob <jadler882@gmail.com>
2023-03-01 20:57:09 -0500

    new

--- 2023-03-01.py
+++ 2023-03-01.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 20:32:05 2023
+
+@author: jadle
+"""
+
 """
 Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list. If there is more than one possible reconstruction, return any of them. If there is no possible reconstruction, then return null.
 
@@ -6,6 +13,20 @@ For example, given the set of words 'quick', 'brown', 'the', 'fox', and the stri
 Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
 """
 
-s = "thequickbrownfox"
-
-print('test')
+def decoder(words, s):
+    words = set(words)
+    res = []
+    
+    n = len(s)
+    i = j = 0
+    
+    while j < n:
+        if s[i:j+1] in words:
+            res.append(s[i:j+1])
+            i = j + 1
+        else:
+            if j == n - 1:
+                return None
+        j += 1
+    
+    return res

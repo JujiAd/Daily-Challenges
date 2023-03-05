c36bf68aecb9cd199982b6f7fe46b45668ca7902
Jacob <jadler882@gmail.com>
2023-03-05 17:17:53 -0500

    daily challenge

--- 2023-03-04.py
+++ 2023-03-04.py
@@ -24,22 +24,44 @@ def finder(s,txt):
     print('False')
     return False
 
-print('First check: ra., raymond')
-finder('ra.','raymond')
-print('\n')
+"""
+I think my understanding of the problem was insufficient to answer it properly. The provided solution is below.
 
-print('Second check: ra., ray')
-finder('ra.','ray')
-print('\n')
+This problem should strike you as recursive. The string should match the regex if we can match the head of the string with the head of the regex and the rest of the string with the rest of the regex. The special characters . and * make implementing this a bit trickier, however, since the * means we can match 0 or any number of characters in the beginning.
 
-print('Third check: .*at, chat')
-finder('.*at','chat')
-print('\n')
+The basic idea then is to do the following. Let's call the string we want to match s and the regex r.
 
-print('Fourth check: .*at, chats')
-finder('.*at','chats')
-print('\n')
+Base case: if r is empty, then return whether s is empty or not.
+Otherwise, if the first thing in r is not proceeded by a *, then match the first character of both r and s, and if they match, return match(r[1:], s[1:]). If they don't, then return false.
+If the first thing in r _is_ proceeded by a *, then try every suffix substring of s on r[2:] and return true if any suffix substring works.
 
-print('Fifth check: he.*o, hellllllo')
-finder('he.*o','hellllllo') 
-print('\n')
+The code should look something like this:
+"""
+
+def matches_first_char(s, r):
+    return s[0] == r[0] or (r[0] == '.' and len(s) > 0)
+
+def matches(s, r):
+    if r == '':
+        return s == ''
+
+    if len(r) == 1 or r[1] != '*':
+        # First character in r is not followed by *
+        if matches_first_char(s,r):
+            return matches(s[1:], r[1:])
+        else:
+            return False
+    
+    else:
+        # First character in r is followed by *
+        # Check zero length
+        if matches(s, r[2:]):
+            return True
+        # If that doesn't match, add more prefixes until the first character of the string doesn't match anymore
+        i = 0
+        while matches_first_char(s[i:], r):
+            if matches(s[i+1:], r[2:]):
+                return True
+            i += 1
+
+            

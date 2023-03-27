ea52bd1a47f7515727adbec031045592110aa4e0
Jacob <jadler882@gmail.com>
2023-03-27 19:46:31 -0400

    new

--- Prob46.py
+++ Prob46.py
@@ -11,7 +11,7 @@ def is_palindrome(s):
 s = 'banana'
 b = "aabcdcb"
 
-# Runs in O(N^2)
+# Runs in O(N^3)
 def findPalindrome(s):
 
     # Check to see if s is a palindrome
@@ -32,3 +32,48 @@ def findPalindrome(s):
     
 
 findPalindrome(b)
+
+# Use dynamic programming to come up with an O(N^2) answer
+"""
+We can improve the running time of this algorithm by using dynamic programming to store any repeated computations. Let's keep an N by N table A, where N is the length of the input string. Then, at each index A[i][j] we'll keep whether or not the substring made from s[i:j] is a palindrome. We'll make use of the following relationships:
+
+All strings of length 1 are palindromes
+s is a palindrome if s[1:-1] is a palindrome and the first and last character of s are the same
+So, when we fill up our table, we can do the following:
+
+First, set each item along the diagonal `A[i:i] to true, since strings of length 1 are always palindromes
+Then, check A[i:i+1] and set it to true if A[i] == A[i + 1] and false otherwise (check all strings of length 2)
+Finally, iterate over the matrix from top to bottom, left to right, only examining the upper diagonal. Note that it doesn't make sense for j to be smaller than i, so that's why we only need to deal with half of the matrix. Set A[i][j] to true only if A[i + 1][j - 1] is true and A[i] == A[j].
+Keep track of the longest palindromic substring we've seen so far.
+"""
+
+def longest_palindrome(s):
+    if not s:
+        return ''
+
+    longest = s[0]
+    # Initialize a None matrix
+    A = [[None for _ in range(len(s))] for _ in range(len(s))]
+
+
+    # Set all substrings of length 1 to be true
+    for i in range(len(s)):
+        A[i][i] = True
+
+
+    # Try all substrings of length 2
+    for i in range(len(s) - 1):
+        A[i][i + 1] = s[i] == s[i + 1]
+
+    i, k = 0, 3
+    while k <= len(s):
+        while i < (len(s) - k + 1) :
+            j = i + k - 1
+            A[i][j] = A[i + 1][j - 1] and s[i] == s[j]
+            # Update longest if necessary
+            if A[i][j] and len(s[i:j + 1]) > len(longest):
+                longest = s[i:j + 1]
+            i += 1
+        k += 1
+        i = 0
+    return longest
---
+++ Prob47.py
@@ -0,0 +1,15 @@
+"""
+Given a array of numbers representing the stock prices of a company in chronological order, write a function that calculates the maximum profit you could have made from buying and selling that stock once. You must buy before you can sell it.
+
+For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could buy the stock at 5 dollars and sell it at 10 dollars.
+"""
+
+def maxProfit(lst):
+
+    maxP = 0
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+            if i < j:
+                tempP = lst[j]-lst[i]
+                maxP = max(maxP,tempP)
+    return maxP
